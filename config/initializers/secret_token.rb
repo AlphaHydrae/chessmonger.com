921e2c6b3ae96c9380a92eeb8a3e8b9507c1d4df
AlphaHydrae <hydrae.alpha@gmail.com>
2012-08-31 23:00:57 +0200

    Initial rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chessmonger::Application.config.secret_token = 'cd0c45d3a4735a016bf2d8867aebafd13050c783e573ff368595cb30fc178843579ce7673cfa24ab705bfc49dc3b3244573caff3d7fc2988819eb3089af62e2a'
