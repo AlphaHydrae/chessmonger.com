6d52f8efb70af35ec8ff19113a016899ebecc073
AlphaHydrae <hydrae.alpha@gmail.com>
2012-10-20 16:29:38 +0200

    Basic board display.

--- app/assets/javascripts/modules/game.js
+++ app/assets/javascripts/modules/game.js
@@ -1,17 +1,76 @@
 
+var Game = Backbone.Model.extend({
+
+  urlRoot : '/games',
+  idAttribute : 'key'
+});
+
 var GameView = Backbone.Marionette.ItemView.extend({
 
   template : 'game',
+  ui : {
+    board : '.board'
+  },
+
+  initialize : function(options) {
+    this.model = new Game({ key : options.config.key });
+    this.model.bind('change', this.update, this);
+  },
 
   onRender : function() {
+    this.drawBoard();
+    this.model.fetch({ dataType : 'json' });
+  },
+
+  update : function() {
+    console.log('update!');
+    this.drawPieces();
+  },
+
+  drawPieces : function() {
+    _.each(this.model.get('board'), _.bind(this.drawPiece, this));
+  },
+
+  drawPiece : function(data) {
+    var pos = data.x + ',' + data.y;
+    var posLayer = this.ui.board.getLayer(pos);
+    var color = data.player == 0 ? 'white' : 'black';
+    this.ui.board.drawImage({
+      source : '/assets/pieces/' + color + '/' + pieceMap[data.piece] + '.png',
+      x : posLayer.x + 5, y : posLayer.y + 5,
+      width : posLayer.width - 10, height : posLayer.height - 10,
+      fromCenter : false
+    });
+  },
+
+  drawBoard : function() {
+    for (var x = 1; x <= 8; x++) {
+      for (var y = 1; y <= 8; y++) {
+        this.ui.board.drawRect({
+          layer : true, name : x + ',' + y, group: 'board',
+          fillStyle : ((x + y) % 2 == 1) ? '#000' : '#fff',
+          x : (x - 1) * 60, y : (8 - y) * 60,
+          width : 60, height : 60,
+          fromCenter : false
+        });
+      }
+    }
   }
 });
+
+var pieceMap = {
+  'ChessPawn' : 'pawn',
+  'ChessKing' : 'king'
+};
 
 $(function() {
 
   $('.game').each(function() {
+    var el = $(this);
     new Backbone.Marionette.Region({
-      el : $(this)
-    }).show(new GameView());
+      el : el
+    }).show(new GameView({
+      config : el.data('config')
+    }));
   });
 });
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -12,4 +12,58 @@ class GamesController < ApplicationController
       render :json => @game.errors.full_messages
     end
   end
+
+  def show
+    respond_to do |format|
+      format.html
+      format.json do
+
+        g = game
+        json = {
+          width: g.rules.board_width,
+          height: g.rules.board_height,
+          actions: g.rules.current_actions(g)
+        }
+
+        json[:actions] = []
+        g.rules.current_actions(g).each do |action|
+          json[:actions] << {
+            :player => g.players.index(action.player),
+            :origin => {
+              :x => action.origin.x,
+              :y => action.origin.y
+            },
+            :target => {
+              :x => action.target.x,
+              :y => action.target.y
+            }
+          }
+        end
+
+        json[:board] = []
+        armory = Chessmonger.rulebook.config.variant('InternationalChess').armory
+        g.board.each do |piece,pos|
+          json[:board] << {
+            :piece => armory.identify(piece.behavior),
+            :player => g.players.index(piece.player),
+            :x => pos.x,
+            :y => pos.y
+          }
+        end
+
+        render :json => json
+      end
+    end
+  end
+
+  private
+
+  def game
+    p1 = Chessmonger::Player.new 'John Doe'
+    p2 = Chessmonger::Player.new 'Jane Doe'
+    rules = Chessmonger::Variants::InternationalChess.new
+    game = Chessmonger::Game.new rules, [ p1, p2 ]
+    rules.setup game
+    game
+  end
 end
